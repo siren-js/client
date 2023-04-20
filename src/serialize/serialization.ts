33519f1c8911f501366d4c88b101fddc9e657d86
Dillon Redding <dillonredding@gmail.com>
2023-04-20 07:50:11 -0500

    Fix multipart form submission (#26)

--- src/serialize/serialization.ts
+++ src/serialize/serialization.ts
@@ -1,7 +1,12 @@
 /**
- * Result of serializing and {@link Action}
+ * Result of serializing an {@link Action}
  */
 export interface Serialization {
-  contentType: string;
   content: BodyInit;
+
+  /**
+   * Media type of `content`. Omit this value to let the Fetch API generate the
+   * `Content-Type` header.
+   */
+  contentType?: string;
 }
